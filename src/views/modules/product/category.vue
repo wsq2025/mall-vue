9f273683003dcbedd06358af0d2e16b72cd3baa0
wsq <673372988@qq.com>
2020-09-21 23:07:45 +0800

    提交

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,115 @@
+<!--  -->
+<template>
+<div class=''>
+    <el-tree show-checkbox node-key="catId" :data="menus" :props="defaultProps" 
+    :expand-on-click-node="false" :default-expanded-keys="expandkey">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+</div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    return {
+      menus: [],
+      expandkey:[],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("成功获取到菜单数据", data.data);
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+        console.log("append",data);
+      },
+
+      remove(node, data) {
+        /* const parent = node.parent;
+        const children = parent.data.children || parent.data;
+        const index = children.findIndex(d => d.id === data.id);
+        children.splice(index, 1); */
+        this.$confirm('是否删除','提示',{
+          confirmButtonText: "确定",
+          cancenButtonText: "取消",
+          type: "warning"
+        }).then(()=>{
+          var ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids,false)
+          }).then(({data}) =>{
+              this.$message({
+                message: "菜单删除成功",
+                type: success
+              });
+            this.getMenus();
+            this.expandkey = [data.parent.dara.catId];
+          });
+          console.log("remove",node,data);
+        }).catch(()=>{
+
+        })
+        
+      }
+  },
+
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    this.getMenus();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+</style>
